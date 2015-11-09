762610d769b1d39a620e29f240823642916145e9
jsong <archenus@gmail.com>
2015-11-09 17:13:42 +0900

    prototype of majong

---
+++ main.py
@@ -0,0 +1,18 @@
+# init the majong to '0'
+dict = {}
+
+limit_up = 0
+limit_down = 0
+limit_left = 0
+limit_right = 0
+for i in range(25):
+    up = -1
+    down = -1
+    left = -1
+    right = -1
+    if i-5 > up
+        up = 0
+    dict[i] = {"value": 0, "up": }
+    print dict[i]["value"]
+    
+    
